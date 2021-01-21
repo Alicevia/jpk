687a459e3dc21de926d9a08851d621b2aa05ff55
Alicevia <ed-phoenix@qq.com>
2021-01-21 20:06:08 +0800

    'init'

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = function (api) {
+  api.cache(true)
+
+  const presets = [
+    [
+      'next/babel',
+      {
+        'preset-env': {
+          useBuiltIns: 'usage',
+          corejs: { version: 3, proposals: true },
+        },
+        // https://babeljs.io/docs/en/babel-plugin-proposal-class-properties
+        'class-properties': { loose: true },
+      },
+    ],
+  ]
+  // const plugins = [ ];
+
+  return {
+    presets,
+    // plugins
+  }
+}
